442d386c132507df4f285f8c729bd1f09aabe763
Sigrid Keydana <keydana@gmx.de>
2019-03-21 17:42:50 +0100

    tfb_ instead of generics for bijectors

--- tests/testthat/test-bijectors.R
+++ tests/testthat/test-bijectors.R
@@ -46,7 +46,7 @@ test_succeeds("Use masked autoregressive flow with template", {
   x <- maf %>% tfd_sample() %>% tensor_value()
 })
 
-test_succeeds("Use an inverse autoregressive flow", {
+test_succeeds("Use an tfb_inverse autoregressive flow", {
   skip_if_not_eager()
   dims <- 5L
   iaf <- tfd_transformed(
@@ -92,8 +92,8 @@ test_succeeds("Use real NVP with template", {
 test_succeeds("Define a reciprocal bijector", {
   b <- tfb_reciprocal()
   x <- c(1, 2, 3)
-  y <- b %>% forward(x)
-  expect_equivalent(b %>% inverse(y) %>% tensor_value(), x)
+  y <- b %>% tfb_forward(x)
+  expect_equivalent(b %>% tfb_inverse(y) %>% tensor_value(), x)
 })
 
 test_succeeds("Define a matvec_lu bijector", {
@@ -145,35 +145,35 @@ test_succeeds("Define a matvec_lu bijector", {
 test_succeeds("Define a scale_tri_l bijector", {
   b <- tfb_scale_tri_l(tfb_exp(), NULL)
   x <- c(0, 0, 0)
-  expect_equal(b %>% forward(x) %>% tensor_value(), diag(2))
+  expect_equal(b %>% tfb_forward(x) %>% tensor_value(), diag(2))
   y <- matrix(c(1, 0, .5, 2), byrow = TRUE, ncol = 2)
-  expect_equivalent(b %>% inverse(y) %>% tensor_value(), c(log(2), .5, log(1)), tol = 1e6)
+  expect_equivalent(b %>% tfb_inverse(y) %>% tensor_value(), c(log(2), .5, log(1)), tol = 1e6)
 })
 
 test_succeeds("Define a sinh_arcsinh bijector", {
   b <- tfb_sinh_arcsinh()
   x <- c(0, 1, 2)
-  expect_equal(b %>% forward(x) %>% tensor_value(),
-               tfb_identity() %>% forward(x) %>% tensor_value())
+  expect_equal(b %>% tfb_forward(x) %>% tensor_value(),
+               tfb_identity() %>% tfb_forward(x) %>% tensor_value())
 })
 
 test_succeeds("Define a softmax_centered bijector", {
   b <- tfb_softmax_centered()
   x <- tf$log(c(2, 3, 4))
   y <- c(0.2, 0.3, 0.4, 0.1)
-  expect_equivalent(b %>% forward(x) %>% tensor_value(), y)
-  expect_equivalent(b %>% inverse(y) %>% tensor_value(), x %>% tensor_value())
+  expect_equivalent(b %>% tfb_forward(x) %>% tensor_value(), y)
+  expect_equivalent(b %>% tfb_inverse(y) %>% tensor_value(), x %>% tensor_value())
 })
 
 test_succeeds("Define a permute bijector", {
   b <- tfb_permute(permutation = list(1, 3, 0, 2))
   x <- seq(0, 1, by = 0.3)
-  y <- b %>% forward(x)
+  y <- b %>% tfb_forward(x)
   expect_equivalent(y %>% tensor_value(), c(0.3, 0.9, 0, 0.6), tol = 1e6)
 
   b <- tfb_permute(permutation = list(2, 1, 0), axis = -2)
   x <- matrix(1:6, ncol = 2, byrow = TRUE)
-  y <- b %>% forward(x)
+  y <- b %>% tfb_forward(x)
   expect_equivalent(y %>% tensor_value(), rbind(x[3,], x[2,], x[1,]), tol = 1e6)
 })
 
@@ -181,51 +181,51 @@ test_succeeds("Define a power transform bijector", {
   power <- 2
   b <- tfb_power_transform(power = power)
   x <- c(1, 2, 3)
-  y <- b %>% forward(x)
+  y <- b %>% tfb_forward(x)
   expect_equivalent(y %>% tensor_value(), (1 + x * power) ^ (1 / power), tol = 1e6)
 })
 
 test_succeeds("Define a normal_cdf bijector", {
   b <- tfb_normal_cdf()
   x <- matrix(c(1, 0, 2, 1), ncol = 2, byrow = TRUE)
-  y <- b %>% forward(x)
-  expect_equal(b %>% inverse(y) %>% tensor_value(), x)
+  y <- b %>% tfb_forward(x)
+  expect_equal(b %>% tfb_inverse(y) %>% tensor_value(), x)
 })
 
-test_succeeds("Define a matrix_inverse_tri_l bijector", {
+test_succeeds("Define a tfb_inverse_tri_l bijector", {
   b <- tfb_matrix_inverse_tri_l()
   x <- matrix(c(1, 0, 2, 1), ncol = 2, byrow = TRUE)
   y <- matrix(c(1, 0,-2, 1), ncol = 2, byrow = TRUE)
-  expect_equal(b %>% forward(x) %>% tensor_value(), y)
+  expect_equal(b %>% tfb_forward(x) %>% tensor_value(), y)
 })
 
 test_succeeds("Define an identity bijector", {
   b <- tfb_identity()
   x <- matrix(1:4, ncol = 2, byrow = TRUE)
-  expect_equal(b %>% forward(x) %>% tensor_value(),
-               b %>% inverse(x) %>% tensor_value())
+  expect_equal(b %>% tfb_forward(x) %>% tensor_value(),
+               b %>% tfb_inverse(x) %>% tensor_value())
 
 })
 
 test_succeeds("Define a sigmoid bijector", {
   b <- tfb_sigmoid()
   x <- matrix(1.1:4.1, ncol = 2, byrow = TRUE)
-  y <- b %>% forward(x)
-  expect_equal((b %>% forward_log_det_jacobian(y, event_ndims = 0))$shape$ndims, 2)
+  y <- b %>% tfb_forward(x)
+  expect_equal((b %>% tfb_forward_log_det_jacobian(y, event_ndims = 0))$shape$ndims, 2)
 })
 
 test_succeeds("Define an exp bijector", {
   b <- tfb_exp()
   x <- matrix(1.1:4.1, ncol = 2, byrow = TRUE)
-  y <- b %>% forward(x)
-  expect_equal(b %>% inverse(y), x %>% tf$convert_to_tensor())
+  y <- b %>% tfb_forward(x)
+  expect_equal(b %>% tfb_inverse(y), x %>% tf$convert_to_tensor())
 })
 
 test_succeeds("Define an absolute value bijector", {
   b <- tfb_absolute_value()
   x <- -1.1
-  y <- b %>% forward(x)
-  expect_equal(b %>% inverse(y) %>% length(), 2)
+  y <- b %>% tfb_forward(x)
+  expect_equal(b %>% tfb_inverse(y) %>% length(), 2)
 })
 
 test_succeeds("Define an affine bijector", {
@@ -236,8 +236,8 @@ test_succeeds("Define an affine bijector", {
       dtype = tf$float32
     )
   x <- c(100, 1000)
-  y <- b %>% forward(x)
-  expect_equal(b %>% inverse(y) %>% length(), 2)
+  y <- b %>% tfb_forward(x)
+  expect_equal(b %>% tfb_inverse(y) %>% length(), 2)
 })
 
 test_succeeds("Define an affine linear operator bijector", {
@@ -245,15 +245,15 @@ test_succeeds("Define an affine linear operator bijector", {
     tfb_affine_linear_operator(shift = c(-1, 0, 1),
                                scale = tf$linalg$LinearOperatorDiag(c(1, 2, 3)))
   x <- c(100, 1000, 10000)
-  y <- b %>% forward(x)
-  expect_equal(b %>% inverse(y) %>% length(), 3)
+  y <- b %>% tfb_forward(x)
+  expect_equal(b %>% tfb_inverse(y) %>% length(), 3)
 })
 
 test_succeeds("Define an affine scalar bijector", {
   b <- tfb_affine_scalar(shift = 3.33)
   x <- c(100, 1000, 10000)
-  y <- b %>% forward(x)
-  expect_equal(b %>% inverse_log_det_jacobian(y, event_ndims = 0) %>% tensor_value(),
+  y <- b %>% tfb_forward(x)
+  expect_equal(b %>% tfb_inverse_log_det_jacobian(y, event_ndims = 0) %>% tensor_value(),
                0)
 })
 
@@ -264,7 +264,7 @@ test_succeeds("Define a batch norm bijector", {
   y <-
     tfd_normal(loc = 1, scale = 2) %>% tfd_sample(100)  # ~ N(1, 2)
   # normalizes using the mean and standard deviation of the current minibatch.
-  x <- dist$bijector %>% inverse(y)  # ~ N(0, 1)
+  x <- dist$bijector %>% tfb_inverse(y)  # ~ N(0, 1)
   expect_equal(x$shape, y$shape)
 })
 
@@ -273,7 +273,7 @@ test_succeeds("Define a blockwise bijector", {
   b <-
     tfb_blockwise(list(tfb_exp(), tfb_sigmoid()), block_sizes = list(2, 1))
   x <- matrix(rep(23, 5 * 3), ncol = 3)
-  y <- b %>% forward(x)
+  y <- b %>% tfb_forward(x)
   expect_equal(y$shape %>% length(), 2)
 })
 
@@ -286,8 +286,8 @@ test_succeeds("Define a Cholesky outer product bijector", {
   b <- tfb_cholesky_outer_product()
   x <- matrix(c(1, 0, 2, 1), ncol = 2, byrow = TRUE)
   y <- matrix(c(1, 2, 2, 5), byrow = TRUE, nrow = 2)
-  expect_equal(b %>% forward(x) %>% tensor_value(), y)
-  expect_equal(b %>% inverse(y) %>% tensor_value(), x)
+  expect_equal(b %>% tfb_forward(x) %>% tensor_value(), y)
+  expect_equal(b %>% tfb_inverse(y) %>% tensor_value(), x)
 })
 
 test_succeeds("Define a Cholesky to inverse Cholesky bijector", {
@@ -301,41 +301,41 @@ test_succeeds("Define a Cholesky to inverse Cholesky bijector", {
     tfb_cholesky_outer_product()
   ))
   x <- matrix(c(1, 0, 2, 1), ncol = 2, byrow = TRUE)
-  expect_equal(b %>% forward(x) %>% tensor_value(),
-               chain %>% forward(x) %>% tensor_value())
+  expect_equal(b %>% tfb_forward(x) %>% tensor_value(),
+               chain %>% tfb_forward(x) %>% tensor_value())
 })
 
 test_succeeds("Define a discrete cosine transform bijector", {
   b <- tfb_discrete_cosine_transform()
   x <- matrix(runif(100))
-  y <- b %>% forward(x)
-  expect_equal(x, b %>% inverse(y) %>% tensor_value(), tolerance = 1e-6)
+  y <- b %>% tfb_forward(x)
+  expect_equal(x, b %>% tfb_inverse(y) %>% tensor_value(), tolerance = 1e-6)
 })
 
 test_succeeds("Define an expm1 bijector", {
   b <- tfb_expm1()
   chain <- tfb_chain(list(tfb_affine_scalar(shift = -1), tfb_exp()))
   x <- matrix(1.1:4.1, ncol = 2, byrow = TRUE)
-  expect_equal(b %>% forward(x) %>% tensor_value(),
-               chain %>% forward(x) %>% tensor_value(),
+  expect_equal(b %>% tfb_forward(x) %>% tensor_value(),
+               chain %>% tfb_forward(x) %>% tensor_value(),
                tol = 1e6)
 
   b <- tfb_expm1()
   x <- matrix(1:8, nrow = 4, byrow = TRUE)
-  y <- b %>% forward(x)
-  expect_equal(b %>% inverse(y), tf$log1p(tf$cast(x, tf$float32)))
+  y <- b %>% tfb_forward(x)
+  expect_equal(b %>% tfb_inverse(y), tf$log1p(tf$cast(x, tf$float32)))
 })
 
 test_succeeds("Define a fill_triangular bijector", {
   b <- tfb_fill_triangular()
   x <- 1:6
-  expect_equal((b %>% forward(x))$shape$ndims, 2)
+  expect_equal((b %>% tfb_forward(x))$shape$ndims, 2)
 })
 
 test_succeeds("Define a Gumbel bijector", {
   b <- tfb_gumbel()
   x <- runif(6)
-  expect_equal(b %>% forward(x) %>% tensor_value(),
+  expect_equal(b %>% tfb_forward(x) %>% tensor_value(),
                tf$exp(-tf$exp(-x)) %>% tensor_value())
 })
 
@@ -348,24 +348,24 @@ test_succeeds("Define an inline bijector", {
     forward_min_event_ndims = 0
   )
   x <- runif(6)
-  expect_equal(b %>% forward(x) %>% tensor_value(),
-               tfb_exp() %>% forward(x) %>% tensor_value())
+  expect_equal(b %>% tfb_forward(x) %>% tensor_value(),
+               tfb_exp() %>% tfb_forward(x) %>% tensor_value())
 })
 
 test_succeeds("Define an invert bijector", {
   inner <- tfb_identity()
   b <- tfb_invert(inner)
   x <- runif(6)
-  expect_equal(b$inverse(x), inner %>% forward(x))
+  expect_equal(b$inverse(x), inner %>% tfb_forward(x))
   # inverse() takes 2 positional arguments but 3 were given
-  # "inverse" from inverse.tensorflow_probability.python.bijectors.bijector.Bijector
-  #expect_equal(b %>% inverse(x), inner %>% forward(x))
+  # if doing
+  # expect_equal(b %>% tfb_inverse(x), inner %>% tfb_forward(x))
 })
 
 test_succeeds("Define a kumaraswamy bijector", {
   b <- tfb_kumaraswamy(concentration1 = 2, concentration0 = 0.3)
   x <- runif(1)
-  expect_lt(b %>% forward(x) %>% tensor_value(), 1)
+  expect_lt(b %>% tfb_forward(x) %>% tensor_value(), 1)
 })
 
 
@@ -373,8 +373,8 @@ test_succeeds("Define a kumaraswamy bijector", {
 test_succeeds("Define an ordered bijector", {
   b <- tfb_ordered()
   x <- seq(0, 1, by = 0.1)
-  y <- b %>% forward(x)
-  expect_equivalent(b %>% inverse(y) %>% tensor_value(), x, tol = 1e6)
+  y <- b %>% tfb_forward(x)
+  expect_equivalent(b %>% tfb_inverse(y) %>% tensor_value(), x, tol = 1e6)
 })
 
 test_succeeds("Define a softplus bijector", {
@@ -382,8 +382,8 @@ test_succeeds("Define a softplus bijector", {
   # " works only on Tensors with 1 batch ndim and 2 event ndims (i.e., vector of matrices)."
   b <- tfb_softplus()
   x <- matrix(1:8, ncol = 2, byrow = TRUE)
-  y <- b %>% forward(x)
-  rev_y <- b %>% inverse(y)
+  y <- b %>% tfb_forward(x)
+  rev_y <- b %>% tfb_inverse(y)
   expect_equivalent(y %>% tensor_value(), log(1 + exp(x)), tol = 1e6)
   expect_equivalent(rev_y %>% tensor_value(), log(exp(x) - 1), tol = 1e6)
 })
@@ -391,14 +391,14 @@ test_succeeds("Define a softplus bijector", {
 test_succeeds("Define a softplus bijector", {
   b <- tfb_softsign()
   x <- matrix(1:8, ncol = 2, byrow = TRUE)
-  expect_equivalent(b %>% forward(x) %>% tensor_value(), x / (1 + abs(x)), tol = 1e6)
-  expect_equivalent(b %>% inverse(x) %>% tensor_value(), x / (1 - abs(x)), tol = 1e6)
+  expect_equivalent(b %>% tfb_forward(x) %>% tensor_value(), x / (1 + abs(x)), tol = 1e6)
+  expect_equivalent(b %>% tfb_inverse(x) %>% tensor_value(), x / (1 - abs(x)), tol = 1e6)
 })
 
 test_succeeds("Define a square bijector", {
   b <- tfb_square()
   x <- matrix(1:8, ncol = 2, byrow = TRUE)
-  expect_equivalent(b %>% forward(x) %>% tensor_value(), x ^ 2)
+  expect_equivalent(b %>% tfb_forward(x) %>% tensor_value(), x ^ 2)
 })
 
 test_succeeds("Define a tanh bijector", {
@@ -410,42 +410,42 @@ test_succeeds("Define a tanh bijector", {
       tfb_affine(scale_identity_multiplier = 2)
     ))
   x <- matrix(1:8, ncol = 2, byrow = TRUE)
-  expect_equal(b %>% forward(x) %>% tensor_value(),
-               chain %>% forward(x) %>% tensor_value(),
+  expect_equal(b %>% tfb_forward(x) %>% tensor_value(),
+               chain %>% tfb_forward(x) %>% tensor_value(),
                tol = 1e6)
 })
 
 test_succeeds("Define a transform_diagonal bijector", {
   b <- tfb_transform_diagonal(tfb_exp())
   x <- diag(2)
-  expect_equivalent(b %>% forward(x) %>% tensor_value(), exp(1) * x, tol = 1e8)
+  expect_equivalent(b %>% tfb_forward(x) %>% tensor_value(), exp(1) * x, tol = 1e8)
 })
 
 test_succeeds("Define a transpose bijector", {
   b <- tfb_transpose(rightmost_transposed_ndims = 2)
   x <- matrix(1:8, ncol = 2, byrow = TRUE)
-  expect_equivalent(b %>% forward(x) %>% tensor_value(),
+  expect_equivalent(b %>% tfb_forward(x) %>% tensor_value(),
                     matrix(1:8, ncol = 2, byrow = FALSE))
 })
 
 test_succeeds("Define a weibull bijector", {
   b <- tfb_weibull(1.5, 2)
   x <- c(0, 0.1, 0.2)
-  expect_equivalent(b %>% forward(x) %>% tensor_value(),
+  expect_equivalent(b %>% tfb_forward(x) %>% tensor_value(),
                     -tf$math$expm1(-((x / 1.5) ** 2)) %>% tensor_value())
 })
 
 test_succeeds("Define a reshape bijector", {
   b <- tfb_reshape(event_shape_out = c(1,-1))
   x <- c(1, 2)
-  y <- b %>% forward(x)
+  y <- b %>% tfb_forward(x)
   expect_equal(y$shape$as_list(), c(1, 2))
 
   x <- matrix(1:4, ncol = 2, byrow = TRUE)
-  y <- b %>% forward(x)
+  y <- b %>% tfb_forward(x)
   expect_equal(y$shape$as_list(), c(2, 1, 2))
 
   x <- matrix(3:4, ncol = 2, byrow = TRUE)
-  y <- b %>% inverse(x)
+  y <- b %>% tfb_inverse(x)
   expect_equal(y$shape$as_list(), c(2))
 })
