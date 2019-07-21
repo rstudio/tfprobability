578fce3e15df02784659126f57391c2ed411e7d6
skeydan <keydana@gmx.de>
2019-07-21 07:53:03 +0200

    add kl_reverse

--- R/vi-functions.R
+++ R/vi-functions.R
@@ -36,3 +36,34 @@ vi_amari_alpha <-
            name = NULL) {
     tfp$vi$amari_alpha(logu, alpha, self_normalized, name)
   }
+
+
+#' The reverse Kullback-Leibler Csiszar-function in log-space.
+#'
+#' A Csiszar-function is a member of `F = { f:R_+ to R : f convex }`.
+#'
+#' When `self_normalized = TRUE`, the KL-reverse Csiszar-function is `f(u) = -log(u) + (u - 1)`.
+#' When `self_normalized = False` the `(u - 1)` term is omitted.
+#' Observe that as an f-Divergence, this Csiszar-function implies: `D_f[p, q] = KL[q, p]`
+#'
+#' The KL is "reverse" because in maximum likelihood we think of minimizing `q` as in `KL[p, q]`.
+#'
+#' Warning: when self_normalized = True` this function makes non-log-space calculations and may
+#' therefore be numerically unstable for `|logu| >> 0`.
+#'
+#' @param logu `float`-like `Tensor` representing `log(u)` from above.
+#' @param self_normalized `logical` indicating whether `f'(u=1)=0`. When
+#' `f'(u=1)=0` the implied Csiszar f-Divergence remains non-negative even
+#' when `p, q` are unnormalized measures.
+#' @param name name prefixed to Ops created by this function.
+#'
+#' @family vi-functions
+#'
+#' @export
+vi_kl_reverse <-
+  function(logu,
+           self_normalized = FALSE,
+           name = NULL) {
+    tfp$vi$kl_reverse(logu, self_normalized, name)
+           }
+
--- tests/testthat/test-vi-functions.R
+++ tests/testthat/test-vi-functions.R
@@ -2,7 +2,13 @@ context("vi-functions")
 
 source("utils.R")
 
-test_succeeds("amari-alpha works", {
+test_succeeds("amari_alpha works", {
   u <- 1
   expect_equal(vi_amari_alpha(log(u)) %>% tensor_value(),  u * log(u) - (u - 1))
 })
+
+test_succeeds("reverse_kl works", {
+  u <- 1
+  expect_equal(vi_amari_alpha(log(u)) %>% tensor_value(),  - u * log(u) + (u - 1))
+})
+
