d8374917f3186c58ae4c7654a3f00b8cc9c82e4b
skeydan <keydana@gmx.de>
2019-07-21 08:15:54 +0200

    add kl_forward

--- tests/testthat/test-vi-functions.R
+++ tests/testthat/test-vi-functions.R
@@ -3,12 +3,19 @@ context("vi-functions")
 source("utils.R")
 
 test_succeeds("amari_alpha works", {
-  u <- 1
-  expect_equal(vi_amari_alpha(log(u)) %>% tensor_value(),  u * log(u) - (u - 1))
+  u <- 2
+  logu <- log(u)
+  expect_equal(vi_amari_alpha(logu, self_normalized = TRUE) %>% tensor_value(),  u * logu - (u - 1))
 })
 
-test_succeeds("reverse_kl works", {
-  u <- 1
-  expect_equal(vi_amari_alpha(log(u)) %>% tensor_value(),  - u * log(u) + (u - 1))
+test_succeeds("kl_reverse works", {
+  u <- 2
+  logu <- log(u)
+  expect_equal(vi_kl_reverse(logu, self_normalized = TRUE) %>% tensor_value(),  -logu + (u - 1))
 })
 
+test_succeeds("kl_forward works", {
+  u <- 2
+  logu <- log(u)
+  expect_equal(vi_kl_forward(logu, self_normalized = TRUE) %>% tensor_value(),  u * log(u) - (u - 1))
+})
