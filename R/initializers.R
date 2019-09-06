3bd1d97107335644b933aaf19683678e4b7d26b9
Daniel Falbel <dfalbel@gmail.com>
2019-09-06 11:41:08 -0300

    adds the blockwise initializer

---
+++ R/initializers.R
@@ -0,0 +1,19 @@
+#' Blockwise Initializer
+#'
+#' Initializer which concats other intializers
+#'
+#' @param initializers list of Keras initializers, eg: [keras::initializer_glorot_uniform()]
+#'  or [initializer_constant()].
+#' @param sizes list of integers scalars representing the number of elements associated
+#'  with each initializer in `initializers`.
+#' @param validate_args bool indicating we should do (possibly expensive) graph-time
+#'  assertions, if necessary.
+#'
+#' @export
+initializer_blockwise <- function(initializers, sizes, validate_args = FALSE) {
+  tfp$layers$BlockwiseInitializer(
+    initializers = initializers,
+    sizes = lapply(sizes, as.integer),
+    validate_args = validate_args
+  )
+}
---
+++ tests/testthat/test-initializers.R
@@ -0,0 +1,16 @@
+context("tensorflow probability keras initializers")
+
+source("utils.R")
+
+test_succeeds("initializer_blockwise works", {
+
+  init <- initializer_blockwise(
+    initializers = lapply(1:5, keras::initializer_constant),
+    sizes = rep(1, 5)
+  )
+
+  layer <- keras::layer_dense(units = 5, input_shape = 1, kernel_initializer = init)
+  layer$build(input_shape = 1)
+
+  expect_equivalent(as.numeric(keras::get_weights(layer)[[1]]), 1:5)
+})
