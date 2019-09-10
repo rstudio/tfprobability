a907d3283252100ded76b1558afc0760e3c144d1
Daniel Falbel <dfalbel@gmail.com>
2019-09-10 21:09:02 +0000

    cast to float64

--- tests/testthat/test-sts-functions.R
+++ tests/testthat/test-sts-functions.R
@@ -52,7 +52,8 @@ test_succeeds("sts_build_factored_variational_loss works", {
 
 test_succeeds("sts_fit_with_hmc works", {
   observed_time_series <-
-    rep(c(3.5, 4.1, 4.5, 3.9, 2.4, 2.1, 1.2), 5) + rep(c(1.1, 1.5, 2.4, 3.1, 4.0), each = 7)
+    rep(c(3.5, 4.1, 4.5, 3.9, 2.4, 2.1, 1.2), 5) + rep(c(1.1, 1.5, 2.4, 3.1, 4.0), each = 7) %>%
+    tensorflow::tf$convert_to_tensor(dtype = tensorflow::tf$float64)
 
   day_of_week <-
     observed_time_series %>% sts_seasonal(num_seasons = 7)
