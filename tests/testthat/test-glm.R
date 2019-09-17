4a276c2020ccaecb1afbcb2b1c656a4901ba2e68
Daniel Falbel <dfalbel@gmail.com>
2019-09-17 15:51:47 -0300

    add more expectations

--- tests/testthat/test-glm.R
+++ tests/testthat/test-glm.R
@@ -4,7 +4,10 @@ test_succeeds("glm_fit.tensorflow.tensor works", {
   y <- rnorm(50, mean = rowSums(x), sd = 0.2)
 
   model <- glm_fit(x, y, model = tfp$glm$Normal())
+  model_r <- glm(y ~ 0 + x[,1] + x[,2])
 
+  expect_equivalent(as.numeric(model[[1]]), model_r$coefficients)
+  expect_s3_class(model, "glm_fit")
 })
 
 
