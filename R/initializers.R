844dc79005dacf87f5684cc6f6342e70a7f34e72
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-08-20 11:59:56 -0400

    Rd fixes

--- R/initializers.R
+++ R/initializers.R
@@ -3,7 +3,7 @@
 #' Initializer which concats other intializers
 #'
 #' @param initializers list of Keras initializers, eg: [keras::initializer_glorot_uniform()]
-#'  or [initializer_constant()].
+#'  or [keras::initializer_constant()].
 #' @param sizes list of integers scalars representing the number of elements associated
 #'  with each initializer in `initializers`.
 #' @param validate_args bool indicating we should do (possibly expensive) graph-time
